369edd4c903217372995318707432558929f3143
Prasad Kumbhar <prasadkumbhar9565@gmail.com>
2026-04-03 15:56:07 +0530

    initial commit

---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module DS_Using_Java {
+}
---
+++ src/pk/DS/StackArr.java
@@ -0,0 +1,10 @@
+package pk.DS;
+
+public class StackArr {
+
+	public static void main(String[] args) {
+		System.out.println("Data Structure");
+
+	}
+
+}
